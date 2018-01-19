c0c072ee986ce16dfb478143d76555a955b049d6
bradleypick <bradleypick@gmail.com>
2018-01-18 20:22:45 -0800

    add reactive dataset and change plot

--- src/server.R
+++ src/server.R
@@ -2,33 +2,43 @@
 #
 # Bradley Pick January 17, 2018
 #
-#
-#
-
-library(shiny)
-library(readr)
-library(plotly)
-
-food_groups <- c("Bakery products",
-                 "Cereal grains and cereal products",
-                 "Fruit, fruit preparations and nuts",
-                 "Vegetables and vegetable preparations",
-                 "Dairy products and eggs",
-                 "Meat",
-                 "Fish and seafood",
-                 "Non-alcoholic beverages and other food products")
-
-data <- read_csv("../data/cansim-food-exp.csv")
 
 server <- function(input, output) {
   
-  output$donutPlot <- renderPlotly({
+  filtered_data <- reactive({
+    
     data %>% 
-      filter(SUMMARY %in% food_groups) %>% 
-      filter(Ref_Date == input$yearInput) %>% 
-      group_by(SUMMARY) %>% 
-      plot_ly(labels = ~SUMMARY, values = ~Value) %>% 
-      add_pie(hole = 0.5)
+      filter(Ref_Date >= min(input$yearInput)) %>% 
+      filter(Ref_Date <= max(input$yearInput)) %>%
+      filter(GEO %in% input$geoInput) %>% 
+      filter(SUMMARY %in% input$foodgroupID)  
+      
+    
   })
+  
+  output$barPlot <- renderPlot({
+    
+    filtered_data() %>% 
+      ggplot(aes(x = SUMMARY, y = Value)) + 
+      geom_bar(aes(fill = as.factor(GEO)), position="dodge", stat="identity") + 
+      scale_fill_brewer("Geography", palette = "Dark2") +
+      scale_x_discrete("Group") + 
+      scale_y_continuous("Dollars", limits=c(0, 1500)) +
+      theme(axis.text.x = element_text(angle = 35, hjust = 1, size=10))
+    
+  },
+  width = 800,
+  height = 600)
+  
+  # output$linePlot <- renderPlot({
+  #   data %>% 
+  #     filter(GEO %in% c("British Columbia")) %>% 
+  #     filter(SUMMARY %in% food_groups) %>% 
+  #     mutate(Value = as.numeric(Value)) %>% 
+  #     ggplot(aes(x = Ref_Date, y = Value, group = SUMMARY)) + 
+  #     geom_line(aes(colour = SUMMARY)) + 
+  #     scale_x_continuous("Date") + 
+  #     scale_y_continuous("Dollars", limits=c(0, 1500)) 
+  # })
   
 }
--- src/ui.R
+++ src/ui.R
@@ -2,42 +2,35 @@
 #
 # Bradley Pick January 17, 2018
 #
-#
-#
-
-library(shiny)
-library(readr)
-
-
-data <- read_csv("../data/cansim-food-exp.csv")
-
-store_rest <- c("Food purchased from stores", 
-                "Food purchased from restaurants")
-
-food_groups <- c("Bakery products",
-                 "Cereal grains and cereal products",
-                 "Fruit, fruit preparations and nuts",
-                 "Vegetables and vegetable preparations",
-                 "Dairy products and eggs",
-                 "Meat",
-                 "Fish and seafood",
-                 "Non-alcoholic beverages and other food products")
 
 ui <- fluidPage(
   
   ## title
-  titlePanel("Food expenditure data"),
+  titlePanel("Statistics Canada Food Expenditure data"),
   
   sidebarPanel(
-    sliderInput("yearInput", "Price", min = 2010, max = 2016,
-                value = 2010, sep=""), 
-    width = 4),
+    
+    sliderInput("yearInput", "Year", min = 2010, max = 2016,
+                value = c(2010, 2016), sep=""), 
+    
+    checkboxGroupInput("geoInput", "Geography",
+                       choices = geographies,
+                       selected = geographies),
+    
+    checkboxGroupInput("foodgroupID", "Food Group", 
+                       choices = food_groups,
+                       selected = food_groups),
+    
+    uiOutput("moreControls")
+    
+    ),
   
   mainPanel(
     
-    plotlyOutput("donutPlot"), 
+    plotOutput("barPlot", width = "100%",
+               hover = hoverOpts(id = "bar_hover", delay = 0)), 
     
-    width = 10
+    plotOutput("linePlot")
     
   )
   
